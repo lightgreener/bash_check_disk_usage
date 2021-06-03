bfca9fc419f054cd81fd8772294b335317fcad83
lightgreener <henry.nanzhang@gmail.com>
2021-06-03 15:13:21 +1000

    all done, it works now

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -11,10 +11,23 @@ for IP in $(awk '/^\//'{print $1}' $HOST_INFO); do
 	USER=$(awk -v ip=$IP 'ip==$1{print $2}'   $HOST_INFO) 
 	PORT=$(awk -v ip=$IP 'ip==$1{print $3}'   $HOST_INFO) 
 	TMP_FILE=/tmp/disk.tmp
-	ssh $
-
-
+	ssh -p $PORT $USER@$IP 'df -h' >$TMP_FILE
+	# same as above
+	# ssh $USER@$IP 'df -h' >$TMP_FILE
+	USAGE_LIST=$(awk '/^\//{print $NF"="int($5)}'  $TMP_FILE)
+	for USAGE in USAGE_LIST; do
+		MOUNTED_NAME=${USAGE%=*}
+		USAGE=${USAGE#*=}
+		if [ $USAGE -gt 80   ]; then
+			echo "$MOUNTED_NAME  partition usage over 80% "
+		
+		else
+			echo " it is fine "
+		fi	
+	done
 done
+
+
 
 
 
